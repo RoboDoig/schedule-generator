cbf8bc4f3d5205eb60ba2b9b24fce3ce56d6f62d
Andrew Erskine <andrew.erskine@crick.ac.uk>
2016-10-04 17:02:12 +0100

    Generate schedule for a more complicated anti vs. corr template

--- Models/ScheduleWidgets.py
+++ Models/ScheduleWidgets.py
@@ -1,7 +1,7 @@
 from PyQt5 import QtWidgets
 import numpy as np
 
-from Designs import simpleCorrDesign
+from Designs import simpleCorrDesign, corrDesign
 from Generation import Gen
 
 
@@ -113,3 +113,76 @@ class SimpleCorrWidget(QtWidgets.QWidget, simpleCorrDesign.Ui_Form):
 
         return params
 
+
+class CorrWidget(QtWidgets.QWidget, corrDesign.Ui_Form):
+    def __init__(self, parentUi=None):
+        super(self.__class__, self).__init__()
+        self.setupUi(self)
+
+        self.parentUi = parentUi
+
+        self.valence_map = None
+
+    def generate_schedule(self, valence_map):
+        lick_fraction = float(self.lickFractionEdit.text())
+        n_valves = len(valence_map)
+
+        n_trials = int(self.nTrialsEdit.text())
+        n_control_trials = int(self.nControlTrialsEdit.text())
+        reward_sequence = Gen.reward_sequence(n_trials + n_control_trials)
+
+        valence_map = np.array(valence_map)
+        valve_index = (np.where(valence_map == 0)[0],
+                       np.where(valence_map == 1)[0],
+                       np.where(valence_map == 2)[0],
+                       np.where(valence_map == 3)[0],
+                       np.where(valence_map == 4)[0])
+
+        frequency = float(self.pulseFrequencyEdit.text())
+        sp_correlated = bool(self.spCorrelatedCheck.isChecked())
+
+        schedule = []
+        for t in range(n_trials + n_control_trials):
+            # set correlation
+            if sp_correlated:
+                correlated = True if reward_sequence[t] == 1 else False
+            else:
+                correlated = False if reward_sequence[t] == 1 else True
+
+            simple_choice = np.random.uniform() > float(self.fractionSimpleTrialsEdit.text())
+
+            if simple_choice:
+                # for a simple choice we will always need 1 o1, 1 o2 valves and 2 b valves all at 50% open
+                o1_valve = np.random.choice(valve_index[1], 1, replace=False) + 1
+                o1_contributions = [0.5]
+
+                o2_valve = np.random.choice(valve_index[2], 1, replace=False) + 1
+                o2_contributions = [0.5]
+
+                b_valve = np.random.choice(valve_index[0], 2, replace=False) + 1
+                b_contributions = [0.5, 0.5]
+            # otherwise there are some differences according to correlation structure
+            else:
+                print(t)
+                # can be made up of random combination of 1 or 2 valves, b valve contrs. add to 1
+                o1_valve = np.random.choice(valve_index[1], np.random.randint(1, 3), replace=False) + 1
+                o1_contributions = np.round(np.random.dirichlet(np.ones(len(o1_valve))) * 0.5, 2)
+
+                o2_valve = np.random.choice(valve_index[2], np.random.randint(1, 3), replace=False) + 1
+                o2_contributions = np.round(np.random.dirichlet(np.ones(len(o2_valve))) * 0.5, 2)
+
+                if correlated:
+                    # if correlated we want our blank valves to add to 1 + we always need 2 valves
+                    b_valve = np.random.choice(valve_index[0], 2, replace=False) + 1
+                    b_contributions = np.round(np.random.dirichlet(np.ones(len(b_valve))), 2)
+                else:
+                    # else they must add to 0.5
+                    b_valve = np.random.choice(valve_index[0], 2, replace=False) + 1
+                    b_contributions = np.round(np.random.dirichlet(np.ones(len(b_valve))) * 0.5, 2)
+
+            schedule.append([reward_sequence[t], correlated, o1_valve, o1_contributions, o2_valve, o2_contributions,
+                             b_valve, b_contributions, frequency, valence_map, lick_fraction])
+
+        return schedule, ['Rewarded', 'Correlated', 'Odour 1 Valve', 'O1 Contributions', 'Odour 2 Valves',
+                          'O2 Contributions', 'Blank Valves', 'B Contributions', 'Frequency',
+                          'Valence Map', 'Lick Fraction']
